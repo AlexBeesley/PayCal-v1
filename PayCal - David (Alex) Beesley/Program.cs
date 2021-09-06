fa1f128186ae0d8abc00932a5bcab904370b49f5
BeesleyD <BeesleyD@IT08561>
2021-09-06 11:46:36 +0100

    Add project files.

---
+++ PayCal - David (Alex) Beesley/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayCal
+{
+    public class payCalMenu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the PayCal System, a Salary Calculator.");
+        }
+    }
+}
